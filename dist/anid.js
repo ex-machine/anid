882eefd7396c35b6a84bc1cbef333c08091fddd0
bisubus <subus.gh@gmail.com>
2018-09-17 23:48:17 +0300

    add dist to commits

---
+++ dist/anid.js
@@ -0,0 +1,48 @@
+(function (root, factory) {
+	/* istanbul ignore next */
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.Anid = factory();
+	}
+})(this, function () {
+
+return function (length, lowercase) {
+	length = length > 24 ? 24 : +length || 8;
+
+	if (lowercase) {
+		return function () {
+			var string = '';
+
+			while (string.length < length) {
+				// 10 to 12 characters, depending on the engine
+				string += Math.random().toString(36).slice(2);
+			}
+
+			return string.slice(0, length);
+		}
+	} else {
+		var symbols = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+		var symbolsCount = 62;
+
+		var maxDecimalString = Math.pow(symbolsCount, length) + '';
+		var maxDecimalLength = +(maxDecimalString.split('e+')[1] || maxDecimalString.length) + 1;
+
+		return function () {
+			var number = +(Math.random().toFixed(maxDecimalLength)).slice(2);
+			var string = '';
+
+			do {
+				string = symbols[number % symbolsCount] + string;
+				number = Math.floor(number / symbolsCount);
+			} while (number > 0 && string.length < length);
+
+			return string;
+		}
+	}
+}
+
+
+});
